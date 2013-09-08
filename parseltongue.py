63287faf51d56c0dd3dece4a68b0d220229260bd
Mark Steve Samson <hello@marksteve.com>
2013-09-08 13:26:10 +0800

    Rewrite

--- parseltongue.py
+++ parseltongue.py
@@ -2,21 +2,18 @@
 parseltongue
 """
 
-from datetime import datetime
 import collections
-import glob
+import json
 import logging
 import os
-import re
-import time
 
 
 from jinja2 import Environment, FileSystemLoader
+from jinja2.exceptions import TemplateNotFound
 from markdown2 import markdown
 
 
 __version__ = '0.0.1'
-POSTED_PAT = re.compile("<!-- posted: (\d+) -->")
 
 
 # Setup logging
@@ -30,164 +27,146 @@ logger.addHandler(logging.StreamHandler())
 
 Config = collections.namedtuple(
     'Config',
-    'posts_dir templates_dir post_template latest_count pages_count',
+    'src_path templates_path',
 )
 
 config = Config(
-    posts_dir='_posts',
-    templates_dir='_templates',
-    post_template='post.html',
-    latest_count=5,
-    pages_count=5,
+    src_path='_src',
+    templates_path='_templates',
 )
 
 
-# Post model
+class Page(object):
 
-class Post(object):
-    """
-    The post model. Post metadata is from the file itself. Much cleaner than
-    adding headers.
-    """
-    def __init__(self, title, body, posted, path, template):
-        self.title = title
-        self.body = body
-        self.posted = posted
-        self.path = path
+    def __init__(self, src_path, template):
+        self.src_path = src_path
         self.template = template
+        with open(src_path, 'r') as f:
+            self.body = markdown(f.read(), extras=['fenced-code-blocks'])
+        self.context = dict()
+
+    def __repr__(self):
+        return "<Page %r>" % self.src_path
 
     @property
-    def post_bn(self):
-        return os.path.basename(self.path).rsplit('.', 1)[0]
+    def is_index(self):
+        return remext(self.src_path).endswith('/index')
 
     @property
-    def is_draft(self):
-        return self.post_bn.startswith('__')
+    def render_path(self):
+        return os.path.join(
+            os.getcwd(),
+            remext(remsrc(self.src_path)) + '.html',
+        )
 
     @property
-    def is_listed(self):
-        return not self.post_bn.startswith('_')
-
-    @property
-    def type(self):
-        return 'post' if self.template.name == config.post_template else 'page'
-
-    @property
-    def url_path(self):
-        if self.type == 'post':
-            url_path = "%s/%s/%s/%s.html" % (
-                self.posted.year,
-                self.posted.month,
-                self.posted.day,
-                self.post_bn,
-            )
-        elif self.type == 'page':
-            url_path = "%s.html" % self.post_bn
-        return url_path
-
-    @classmethod
-    def load(cls, path, template):
-        with open(path, 'r') as f:
-            contents = f.read()
-        title, body = contents.split('\n', 1)
-        body = markdown(body, extras=['fenced-code-blocks'])
-
-        match = POSTED_PAT.search(body)
-        if match:
-            posted_ts = int(match.group(1))
-            posted = datetime.fromtimestamp(posted_ts)
+    def url(self):
+        url = os.path.join('/', remext(remsrc(self.src_path)))
+        if self.is_index:
+            return url[:-len('index')]
         else:
-            posted = datetime.now()
-            with open(path, 'a') as f:
-                posted_ts = int(time.mktime(posted.timetuple()))
-                f.write("<!-- posted: %d -->" % posted_ts)
-
-        return cls(title, body, posted, path, template)
+            return url + '.html'
 
     def render(self):
         # Create parent dirs
         try:
-            os.makedirs(os.path.dirname(self.url_path))
+            os.makedirs(os.path.dirname(self.render_path))
         except OSError:
             pass
 
-        # Render post
-        with open(self.url_path, 'w') as f:
-            f.write(self.template.render(post=self))
+        # Render page
+        with open(self.render_path, 'w') as f:
+            f.write(self.template.render(page=self, **self.context))
 
 
-def load_posts(env):
-    """
-    Loads all posts from posts directory
-    """
-
-    post_template = env.get_template(config.post_template)
-    templates = env.list_templates()
-    posts = []
-
-    for post_path in glob.glob(os.path.join(config.posts_dir, '*.md')):
-        template_name = os.path.basename(post_path).rsplit('.', 1)[0] + '.html'
-        template = (env.get_template(template_name) if template_name in
-                    templates else post_template)
-        posts.append(Post.load(post_path, template))
-
-    logger.info("Loaded %d posts.", len(posts))
-
-    return list(reversed(sorted(posts, key=lambda post: post.posted)))
+def remext(s):
+    return s.rsplit('.', 1)[0]
 
 
-def render_posts(env, posts):
-    """
-    Renders post pages (and listings later)
-    """
-
-    for post in posts:
-        if post.is_draft:
-            logger.info("Skipped %s", post.url_path)
-            continue
-        post.render()
-        logger.info("Rendered %s", post.url_path)
-
-
-def render_index(env, posts):
-    """
-    Renders index page
-    """
-
-    index_template = env.get_template('index.html')
-
-    latest = []
-    pages = []
-    for post in posts:
-        max_latest = len(latest) >= config.latest_count
-        max_pages = len(pages) >= config.pages_count
-        if max_latest and max_pages:
-            break
-        if post.is_listed:
-            if post.type == 'post' and not max_latest:
-                latest.append(post)
-            elif post.type == 'page' and not max_pages:
-                pages.append(post)
-
-    with open('index.html', 'w') as f:
-        f.write(index_template.render(latest=latest, pages=pages))
+def remsrc(s):
+    return s.replace(config.src_path, '').lstrip('/')
 
 
 def main():
     logger.info("\nParseltongue %s", __version__)
 
-    env = Environment(loader=FileSystemLoader(config.templates_dir))
+    env = Environment(loader=FileSystemLoader(config.templates_path))
 
-    logger.info("\nLoading posts...")
-    posts = load_posts(env)
+    def get_template(page_path, dir_path):
+        for path in page_path, dir_path:
+            template_path = remext(remsrc(path)) + '.html'
+            try:
+                return env.get_template(template_path)
+            except TemplateNotFound:
+                continue
 
-    logger.info("\nRendering posts...")
-    render_posts(env, posts)
+    def get_context(path):
+        context_path = remext(path) + '.json'
+        try:
+            with open(context_path) as f:
+                return json.loads(f.read())
+        except IOError:
+            return dict()
 
-    logger.info("\nRendering index page...")
-    render_index(env, posts)
+    context = dict()
+    nav = dict()
 
-    logger.info("\nDone!\n")
+    logger.info('\nPages\n')
+
+    for dir_path, _, filenames in os.walk(config.src_path):
+        pages = []
+        index = None
+
+        # Update context from this level's context
+        context.update(**get_context(os.path.join(dir_path, 'index')))
+
+        # Load pages for this level
+        for filename in filenames:
+            if not (filename.endswith('md') or filename.endswith('markdown')):
+                continue
+
+            page_path = os.path.join(dir_path, filename)
+
+            # Get template
+            template = get_template(page_path, dir_path)
+            if not template:
+                logger.error("Template for %s not found.", page_path)
+                exit(1)
+
+            # Creat page instance
+            page = Page(page_path, template)
+
+            # Update with current context
+            page.context.update(**context)
+
+            if page.is_index:
+                index = page
+            else:
+                page.context.update(get_context(page_path))
+                pages.append(page)
+                logger.info("%s", page_path)
+                logger.info("  Context: %r", page.context)
+
+        if index:
+            index.context.update(pages=pages)
+            logger.info("%s", index.src_path)
+            logger.info("  Context: %r", index.context)
+
+        # Update nav
+        nav[os.path.join('/', remsrc(dir_path))] = dict(
+            index=index,
+            pages=pages,
+        )
+
+    logger.info('\nRender\n')
+
+    # Render all
+    for path, level in nav.iteritems():
+        logger.info("%s", path)
+        for page in level['pages'] + [level['index']]:
+            logger.info("  %s -> %s", page.src_path, page.render_path)
+            page.context.update(nav=nav)
+            page.render()
 
 
 if __name__ == '__main__':
